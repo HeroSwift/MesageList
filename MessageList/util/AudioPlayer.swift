d6ed7b0dacab62a1258721c3f62f7cd00695ca27
musicode <musicode@qq.com>
2018-12-05 14:08:18 +0800

    update

--- MessageList/util/AudioPlayer.swift
+++ MessageList/util/AudioPlayer.swift
@@ -97,6 +97,8 @@ class AudioPlayer: NSObject {
         
         category = AVAudioSession.sharedInstance().category
         
+        // 默认是 AVAudioSessionCategorySoloAmbient
+        // 静音状态下没法播放音频
         useSpeaker()
         
     }
